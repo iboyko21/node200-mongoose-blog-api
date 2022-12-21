d1cf98996659c2d356bfa9a37d59d80c55755a30
Igor Boyko <iboyko@gmail.com>
2022-12-20 16:09:54 -0800

    added User model

---
+++ models/User.js
@@ -0,0 +1,16 @@
+// Imports mongoose and extracts Schema into it's own variable
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Creates a new Mongoose Schema
+const UserSchema = new Schema({
+    firstName: { type: String, required: true }, // firstName property is a string and required
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    social: {
+        facebook: { type: String, required: false },
+        twitter: { type: String, required: false },
+        linkedIn: { type: String, required: false }
+    }
+});
