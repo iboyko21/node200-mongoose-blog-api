9d033ea03bf5eb9a06537066743bf4aea53d0a8d
Igor Boyko <iboyko@gmail.com>
2023-08-01 06:57:42 -0700

    updated server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const server = require('./app');
 
 server.listen(8080, function() {
-    console.log('Server listening on http://localhost:8080');
+    console.log('Server is listening on http://localhost:8080');
 });
